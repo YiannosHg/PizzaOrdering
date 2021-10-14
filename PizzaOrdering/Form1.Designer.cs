cc0921d7d95436b642ae9b66ba25c39b464c1cf3
YiannosHg <48808892+YiannosHg@users.noreply.github.com>
2021-10-14 17:16:19 +0300

    Created functions to calculate total cost

--- PizzaOrdering/Form1.Designer.cs
+++ PizzaOrdering/Form1.Designer.cs
@@ -30,21 +30,24 @@ namespace PizzaOrdering
         private void InitializeComponent()
         {
             this.SizeGroupBox = new System.Windows.Forms.GroupBox();
-            this.IngredientsGroupBox = new System.Windows.Forms.GroupBox();
-            this.smallRadioButton = new System.Windows.Forms.RadioButton();
-            this.mediumRadioButton = new System.Windows.Forms.RadioButton();
-            this.largeRadioButton = new System.Windows.Forms.RadioButton();
-            this.peperoniCheckBox = new System.Windows.Forms.CheckBox();
-            this.onionCheckBox = new System.Windows.Forms.CheckBox();
-            this.mushroomCheckBox = new System.Windows.Forms.CheckBox();
-            this.blackOlivesCheckBox = new System.Windows.Forms.CheckBox();
-            this.pineappleCheckBox = new System.Windows.Forms.CheckBox();
-            this.extraCheeseCheckBox = new System.Windows.Forms.CheckBox();
-            this.orderButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.largeRadioButton = new System.Windows.Forms.RadioButton();
+            this.mediumRadioButton = new System.Windows.Forms.RadioButton();
+            this.smallRadioButton = new System.Windows.Forms.RadioButton();
+            this.IngredientsGroupBox = new System.Windows.Forms.GroupBox();
+            this.ingredientsLabel = new System.Windows.Forms.Label();
+            this.extraCheeseCheckBox = new System.Windows.Forms.CheckBox();
+            this.pineappleCheckBox = new System.Windows.Forms.CheckBox();
+            this.blackOlivesCheckBox = new System.Windows.Forms.CheckBox();
+            this.mushroomCheckBox = new System.Windows.Forms.CheckBox();
+            this.onionCheckBox = new System.Windows.Forms.CheckBox();
+            this.peperoniCheckBox = new System.Windows.Forms.CheckBox();
+            this.orderButton = new System.Windows.Forms.Button();
             this.timeMaskedTextBox = new System.Windows.Forms.MaskedTextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.nOfIn = new System.Windows.Forms.Label();
             this.SizeGroupBox.SuspendLayout();
             this.IngredientsGroupBox.SuspendLayout();
             this.SuspendLayout();
@@ -57,48 +60,19 @@ namespace PizzaOrdering
             this.SizeGroupBox.Controls.Add(this.smallRadioButton);
             this.SizeGroupBox.Location = new System.Drawing.Point(93, 39);
             this.SizeGroupBox.Name = "SizeGroupBox";
-            this.SizeGroupBox.Size = new System.Drawing.Size(200, 113);
+            this.SizeGroupBox.Size = new System.Drawing.Size(235, 113);
             this.SizeGroupBox.TabIndex = 0;
             this.SizeGroupBox.TabStop = false;
             this.SizeGroupBox.Text = "Size";
             // 
-            // IngredientsGroupBox
+            // label1
             // 
-            this.IngredientsGroupBox.Controls.Add(this.label2);
-            this.IngredientsGroupBox.Controls.Add(this.extraCheeseCheckBox);
-            this.IngredientsGroupBox.Controls.Add(this.pineappleCheckBox);
-            this.IngredientsGroupBox.Controls.Add(this.blackOlivesCheckBox);
-            this.IngredientsGroupBox.Controls.Add(this.mushroomCheckBox);
-            this.IngredientsGroupBox.Controls.Add(this.onionCheckBox);
-            this.IngredientsGroupBox.Controls.Add(this.peperoniCheckBox);
-            this.IngredientsGroupBox.Location = new System.Drawing.Point(93, 181);
-            this.IngredientsGroupBox.Name = "IngredientsGroupBox";
-            this.IngredientsGroupBox.Size = new System.Drawing.Size(200, 187);
-            this.IngredientsGroupBox.TabIndex = 1;
-            this.IngredientsGroupBox.TabStop = false;
-            this.IngredientsGroupBox.Text = "Ingredients";
-            // 
-            // smallRadioButton
-            // 
-            this.smallRadioButton.AutoSize = true;
-            this.smallRadioButton.Location = new System.Drawing.Point(7, 40);
-            this.smallRadioButton.Name = "smallRadioButton";
-            this.smallRadioButton.Size = new System.Drawing.Size(50, 17);
-            this.smallRadioButton.TabIndex = 0;
-            this.smallRadioButton.TabStop = true;
-            this.smallRadioButton.Text = "Small";
-            this.smallRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // mediumRadioButton
-            // 
-            this.mediumRadioButton.AutoSize = true;
-            this.mediumRadioButton.Location = new System.Drawing.Point(7, 64);
-            this.mediumRadioButton.Name = "mediumRadioButton";
-            this.mediumRadioButton.Size = new System.Drawing.Size(62, 17);
-            this.mediumRadioButton.TabIndex = 1;
-            this.mediumRadioButton.TabStop = true;
-            this.mediumRadioButton.Text = "Medium";
-            this.mediumRadioButton.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Please select size";
             // 
             // largeRadioButton
             // 
@@ -110,67 +84,124 @@ namespace PizzaOrdering
             this.largeRadioButton.TabStop = true;
             this.largeRadioButton.Text = "Large";
             this.largeRadioButton.UseVisualStyleBackColor = true;
+            this.largeRadioButton.CheckedChanged += new System.EventHandler(this.largeRadioButton_CheckedChanged);
             // 
-            // peperoniCheckBox
+            // mediumRadioButton
             // 
-            this.peperoniCheckBox.AutoSize = true;
-            this.peperoniCheckBox.Location = new System.Drawing.Point(6, 38);
-            this.peperoniCheckBox.Name = "peperoniCheckBox";
-            this.peperoniCheckBox.Size = new System.Drawing.Size(68, 17);
-            this.peperoniCheckBox.TabIndex = 0;
-            this.peperoniCheckBox.Text = "Peperoni";
-            this.peperoniCheckBox.UseVisualStyleBackColor = true;
+            this.mediumRadioButton.AutoSize = true;
+            this.mediumRadioButton.Location = new System.Drawing.Point(7, 64);
+            this.mediumRadioButton.Name = "mediumRadioButton";
+            this.mediumRadioButton.Size = new System.Drawing.Size(62, 17);
+            this.mediumRadioButton.TabIndex = 1;
+            this.mediumRadioButton.TabStop = true;
+            this.mediumRadioButton.Text = "Medium";
+            this.mediumRadioButton.UseVisualStyleBackColor = true;
+            this.mediumRadioButton.CheckedChanged += new System.EventHandler(this.mediumRadioButton_CheckedChanged);
             // 
-            // onionCheckBox
+            // smallRadioButton
             // 
-            this.onionCheckBox.AutoSize = true;
-            this.onionCheckBox.Location = new System.Drawing.Point(6, 62);
-            this.onionCheckBox.Name = "onionCheckBox";
-            this.onionCheckBox.Size = new System.Drawing.Size(54, 17);
-            this.onionCheckBox.TabIndex = 1;
-            this.onionCheckBox.Text = "Onion";
-            this.onionCheckBox.UseVisualStyleBackColor = true;
+            this.smallRadioButton.AutoSize = true;
+            this.smallRadioButton.Location = new System.Drawing.Point(7, 40);
+            this.smallRadioButton.Name = "smallRadioButton";
+            this.smallRadioButton.Size = new System.Drawing.Size(50, 17);
+            this.smallRadioButton.TabIndex = 0;
+            this.smallRadioButton.TabStop = true;
+            this.smallRadioButton.Text = "Small";
+            this.smallRadioButton.UseVisualStyleBackColor = true;
+            this.smallRadioButton.CheckedChanged += new System.EventHandler(this.smallRadioButton_CheckedChanged);
+            // 
+            // IngredientsGroupBox
+            // 
+            this.IngredientsGroupBox.Controls.Add(this.ingredientsLabel);
+            this.IngredientsGroupBox.Controls.Add(this.extraCheeseCheckBox);
+            this.IngredientsGroupBox.Controls.Add(this.pineappleCheckBox);
+            this.IngredientsGroupBox.Controls.Add(this.blackOlivesCheckBox);
+            this.IngredientsGroupBox.Controls.Add(this.mushroomCheckBox);
+            this.IngredientsGroupBox.Controls.Add(this.onionCheckBox);
+            this.IngredientsGroupBox.Controls.Add(this.peperoniCheckBox);
+            this.IngredientsGroupBox.Enabled = false;
+            this.IngredientsGroupBox.Location = new System.Drawing.Point(93, 172);
+            this.IngredientsGroupBox.Name = "IngredientsGroupBox";
+            this.IngredientsGroupBox.Size = new System.Drawing.Size(235, 196);
+            this.IngredientsGroupBox.TabIndex = 1;
+            this.IngredientsGroupBox.TabStop = false;
+            this.IngredientsGroupBox.Text = "Ingredients";
+            // 
+            // ingredientsLabel
+            // 
+            this.ingredientsLabel.AutoSize = true;
+            this.ingredientsLabel.Location = new System.Drawing.Point(6, 19);
+            this.ingredientsLabel.Name = "ingredientsLabel";
+            this.ingredientsLabel.Size = new System.Drawing.Size(124, 13);
+            this.ingredientsLabel.TabIndex = 6;
+            this.ingredientsLabel.Text = "Please select ingredients";
+            // 
+            // extraCheeseCheckBox
+            // 
+            this.extraCheeseCheckBox.AutoSize = true;
+            this.extraCheeseCheckBox.Location = new System.Drawing.Point(6, 175);
+            this.extraCheeseCheckBox.Name = "extraCheeseCheckBox";
+            this.extraCheeseCheckBox.Size = new System.Drawing.Size(88, 17);
+            this.extraCheeseCheckBox.TabIndex = 5;
+            this.extraCheeseCheckBox.Text = "Extra cheese";
+            this.extraCheeseCheckBox.UseVisualStyleBackColor = true;
+            this.extraCheeseCheckBox.CheckedChanged += new System.EventHandler(this.extraCheeseCheckBox_CheckedChanged);
+            // 
+            // pineappleCheckBox
+            // 
+            this.pineappleCheckBox.AutoSize = true;
+            this.pineappleCheckBox.Location = new System.Drawing.Point(6, 151);
+            this.pineappleCheckBox.Name = "pineappleCheckBox";
+            this.pineappleCheckBox.Size = new System.Drawing.Size(73, 17);
+            this.pineappleCheckBox.TabIndex = 4;
+            this.pineappleCheckBox.Text = "Pineapple";
+            this.pineappleCheckBox.UseVisualStyleBackColor = true;
+            this.pineappleCheckBox.CheckedChanged += new System.EventHandler(this.pineappleCheckBox_CheckedChanged);
+            // 
+            // blackOlivesCheckBox
+            // 
+            this.blackOlivesCheckBox.AutoSize = true;
+            this.blackOlivesCheckBox.Location = new System.Drawing.Point(6, 127);
+            this.blackOlivesCheckBox.Name = "blackOlivesCheckBox";
+            this.blackOlivesCheckBox.Size = new System.Drawing.Size(85, 17);
+            this.blackOlivesCheckBox.TabIndex = 3;
+            this.blackOlivesCheckBox.Text = "Black Olives";
+            this.blackOlivesCheckBox.UseVisualStyleBackColor = true;
+            this.blackOlivesCheckBox.CheckedChanged += new System.EventHandler(this.blackOlivesCheckBox_CheckedChanged);
             // 
             // mushroomCheckBox
             // 
             this.mushroomCheckBox.AutoSize = true;
-            this.mushroomCheckBox.Location = new System.Drawing.Point(6, 86);
+            this.mushroomCheckBox.Location = new System.Drawing.Point(6, 103);
             this.mushroomCheckBox.Name = "mushroomCheckBox";
             this.mushroomCheckBox.Size = new System.Drawing.Size(75, 17);
             this.mushroomCheckBox.TabIndex = 2;
             this.mushroomCheckBox.Text = "Mushroom";
             this.mushroomCheckBox.TextAlign = System.Drawing.ContentAlignment.TopLeft;
             this.mushroomCheckBox.UseVisualStyleBackColor = true;
+            this.mushroomCheckBox.CheckedChanged += new System.EventHandler(this.mushroomCheckBox_CheckedChanged);
             // 
-            // blackOlivesCheckBox
+            // onionCheckBox
             // 
-            this.blackOlivesCheckBox.AutoSize = true;
-            this.blackOlivesCheckBox.Location = new System.Drawing.Point(6, 110);
-            this.blackOlivesCheckBox.Name = "blackOlivesCheckBox";
-            this.blackOlivesCheckBox.Size = new System.Drawing.Size(85, 17);
-            this.blackOlivesCheckBox.TabIndex = 3;
-            this.blackOlivesCheckBox.Text = "Black Olives";
-            this.blackOlivesCheckBox.UseVisualStyleBackColor = true;
+            this.onionCheckBox.AutoSize = true;
+            this.onionCheckBox.Location = new System.Drawing.Point(6, 79);
+            this.onionCheckBox.Name = "onionCheckBox";
+            this.onionCheckBox.Size = new System.Drawing.Size(54, 17);
+            this.onionCheckBox.TabIndex = 1;
+            this.onionCheckBox.Text = "Onion";
+            this.onionCheckBox.UseVisualStyleBackColor = true;
+            this.onionCheckBox.CheckedChanged += new System.EventHandler(this.onionCheckBox_CheckedChanged);
             // 
-            // pineappleCheckBox
+            // peperoniCheckBox
             // 
-            this.pineappleCheckBox.AutoSize = true;
-            this.pineappleCheckBox.Location = new System.Drawing.Point(6, 134);
-            this.pineappleCheckBox.Name = "pineappleCheckBox";
-            this.pineappleCheckBox.Size = new System.Drawing.Size(73, 17);
-            this.pineappleCheckBox.TabIndex = 4;
-            this.pineappleCheckBox.Text = "Pineapple";
-            this.pineappleCheckBox.UseVisualStyleBackColor = true;
-            // 
-            // extraCheeseCheckBox
-            // 
-            this.extraCheeseCheckBox.AutoSize = true;
-            this.extraCheeseCheckBox.Location = new System.Drawing.Point(6, 158);
-            this.extraCheeseCheckBox.Name = "extraCheeseCheckBox";
-            this.extraCheeseCheckBox.Size = new System.Drawing.Size(88, 17);
-            this.extraCheeseCheckBox.TabIndex = 5;
-            this.extraCheeseCheckBox.Text = "Extra cheese";
-            this.extraCheeseCheckBox.UseVisualStyleBackColor = true;
+            this.peperoniCheckBox.AutoSize = true;
+            this.peperoniCheckBox.Location = new System.Drawing.Point(6, 55);
+            this.peperoniCheckBox.Name = "peperoniCheckBox";
+            this.peperoniCheckBox.Size = new System.Drawing.Size(68, 17);
+            this.peperoniCheckBox.TabIndex = 0;
+            this.peperoniCheckBox.Text = "Peperoni";
+            this.peperoniCheckBox.UseVisualStyleBackColor = true;
+            this.peperoniCheckBox.CheckedChanged += new System.EventHandler(this.peperoniCheckBox_CheckedChanged);
             // 
             // orderButton
             // 
@@ -181,24 +212,6 @@ namespace PizzaOrdering
             this.orderButton.Text = "Order";
             this.orderButton.UseVisualStyleBackColor = true;
             this.orderButton.Click += new System.EventHandler(this.orderButton_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Please select size";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 19);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Please select ingredients";
             // 
             // timeMaskedTextBox
             // 
@@ -216,11 +229,41 @@ namespace PizzaOrdering
             this.label3.TabIndex = 4;
             this.label3.Text = "Time:";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(315, 377);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Total";
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Location = new System.Drawing.Point(366, 380);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(28, 13);
+            this.priceLabel.TabIndex = 7;
+            this.priceLabel.Text = "0.00";
+            // 
+            // nOfIn
+            // 
+            this.nOfIn.AutoSize = true;
+            this.nOfIn.Location = new System.Drawing.Point(270, 380);
+            this.nOfIn.Name = "nOfIn";
+            this.nOfIn.Size = new System.Drawing.Size(14, 13);
+            this.nOfIn.TabIndex = 8;
+            this.nOfIn.Text = "#";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(526, 488);
+            this.Controls.Add(this.nOfIn);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.timeMaskedTextBox);
             this.Controls.Add(this.orderButton);
@@ -252,9 +295,12 @@ namespace PizzaOrdering
         private System.Windows.Forms.CheckBox peperoniCheckBox;
         private System.Windows.Forms.Button orderButton;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ingredientsLabel;
         private System.Windows.Forms.MaskedTextBox timeMaskedTextBox;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.Label nOfIn;
     }
 }
 
--- PizzaOrdering/Form1.cs
+++ PizzaOrdering/Form1.cs
@@ -12,6 +12,24 @@ namespace PizzaOrdering
 {
     public partial class Form1 : Form
     {
+        // Price of pizza based on its size
+        private const double smallSizeCost = 5.50;
+        private const double mediumSizeCost = 11.75;
+        private const double largeSizeCost = 15.00;
+
+        // Free ingredients based on pizza size
+        private const int smallFreeIngredients = 2;
+        private const int mediumFreeIngredients = 3;
+        private const int largeFreeIngredients = 4;
+
+        // Extra ingredient cost
+        private const double extraIngredientCost = 0.75;
+
+        double pizzaSizeCost = 0;
+        double totalCost = 0;
+        int freeIngredients = 0;
+        int numOfIngredients = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,37 +37,104 @@ namespace PizzaOrdering
 
         private void orderButton_Click(object sender, EventArgs e)
         {
-            int freeIngredients = getSize();
+            
         }
 
-        int getSize()
+        private void smallRadioButton_CheckedChanged(object sender, EventArgs e)
         {
-            int numOfIngredients = 0;
-            if (smallRadioButton.Checked)
-                numOfIngredients = 2;
-            
-            else if (mediumRadioButton.Checked)
-                numOfIngredients = 3;
-            
-            else if (largeRadioButton.Checked)
-                numOfIngredients = 4;
-            
-            return numOfIngredients;
+            IngredientsGroupBox.Enabled = true;
+            pizzaSizeCost = smallSizeCost;
+            freeIngredients = smallFreeIngredients;
+            ingredientsLabel.Text = "Ingredients: Free up to: " + freeIngredients + Environment.NewLine +"0.75 for each extra ingredient";
+            priceLabel.Text = pizzaSizeCost.ToString();
         }
 
-        int getIngredients()
+        private void mediumRadioButton_CheckedChanged(object sender, EventArgs e)
         {
-            int amountOfIngredients = 0;
+            IngredientsGroupBox.Enabled = true;
+            pizzaSizeCost = mediumSizeCost;
+            freeIngredients = mediumFreeIngredients;
+            ingredientsLabel.Text = "Ingredients: Free up to: " + freeIngredients + Environment.NewLine + "0.75 for each extra ingredient";
+            priceLabel.Text = pizzaSizeCost.ToString();
+        }
 
-            if (peperoniCheckBox.Checked)
-                amountOfIngredients += 1;
+        private void largeRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            IngredientsGroupBox.Enabled = true;
+            pizzaSizeCost = largeSizeCost;
+            freeIngredients = largeFreeIngredients;
+            ingredientsLabel.Text = "Ingredients: Free up to: " + freeIngredients + Environment.NewLine + "0.75 for each extra ingredient";
+            priceLabel.Text = pizzaSizeCost.ToString();
+        }
 
-            if (onionCheckBox.Checked)
-                amountOfIngredients += 1;
+        private void peperoniCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (peperoniCheckBox.Checked == false)
+                --numOfIngredients;
+            else
+                ++numOfIngredients;
 
-            if (mushroomCheckBox.Checked)
+            setTotalCost();
+        }
 
-            return amountOfIngredients;
+        private void onionCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (onionCheckBox.Checked == false)
+                --numOfIngredients;
+            else
+                ++numOfIngredients;
+
+            setTotalCost();
+        }
+
+        private void mushroomCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (mushroomCheckBox.Checked == false)
+                --numOfIngredients;
+            else
+                ++numOfIngredients;
+
+            setTotalCost();
+        }
+
+        private void blackOlivesCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (blackOlivesCheckBox.Checked == false)
+                --numOfIngredients;
+            else
+                ++numOfIngredients;
+
+            setTotalCost();
+        }
+
+        private void pineappleCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (pineappleCheckBox.Checked == false)
+                --numOfIngredients;
+            else
+                ++numOfIngredients;
+
+            setTotalCost();
+        }
+
+        private void extraCheeseCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (extraCheeseCheckBox.Checked == false)
+                --numOfIngredients;
+            else
+                ++numOfIngredients;
+
+            setTotalCost();
+        }
+
+        void setTotalCost()
+        {
+            if (numOfIngredients > freeIngredients)
+                totalCost = (numOfIngredients - freeIngredients) * extraIngredientCost + pizzaSizeCost;
+            else
+                totalCost = pizzaSizeCost;
+
+            priceLabel.Text = totalCost.ToString();
         }
     }
 }
